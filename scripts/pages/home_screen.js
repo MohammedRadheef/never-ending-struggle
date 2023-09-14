16a9c1b719ca1f51cc104e0c9669ba7b87a61c7b
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-09-14 09:37:12 +0530

    live updating method added

--- scripts/pages/home_screen.js
+++ scripts/pages/home_screen.js
@@ -5,6 +5,11 @@ const homeMangoScript = new MangoScript('homeUI', function(){
     height: window.innerHeight,
     imageSizeAuto: true,
     mergeImageUrl: false,
-    img: SRC_HOME_BG_IMG.tag
+    img: SRC_HOME_BG_IMG.tag,
   })
+  
+  background_image.data.onupdated = function() {
+    background_image.data.width = window.innerWidth
+    background_image.data.height = window.innerHeight
+  }
 })
