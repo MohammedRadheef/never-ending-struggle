4caf4fdc8a38a6a7c050b3814d2186263e51c72d
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-09-14 09:32:24 +0530

    started home page

--- scripts/pages/home_screen.js
+++ scripts/pages/home_screen.js
@@ -1,5 +1,10 @@
 const homeMangoScript = new MangoScript('homeUI', function(){
-  setTimeout(function(){
-    console.log(canvas.entityStore);
-  }, 500)
+  var background_image = new entity({
+    type: 'image',
+    width: window.innerWidth,
+    height: window.innerHeight,
+    imageSizeAuto: true,
+    mergeImageUrl: false,
+    img: SRC_HOME_BG_IMG.tag
+  })
 })
--- scripts/property/common.sources.js
+++ scripts/property/common.sources.js
@@ -1,4 +1,6 @@
 const commonSources = []
 const SRC_ICON_SHEET = new GameSource('sheet', 'images/ui/icons.sheet')
+const SRC_HOME_BG_IMG = new GameSource('image', 'images/thumbnail/home.jpg')
 
 commonSources.push(SRC_ICON_SHEET)
+commonSources.push(SRC_HOME_BG_IMG)
