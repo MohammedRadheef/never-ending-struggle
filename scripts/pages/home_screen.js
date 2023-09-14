825027aaabb404ae1bb0462d4dfd50b25a67e23a
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-09-14 10:09:20 +0530

    icons added

--- scripts/pages/home_screen.js
+++ scripts/pages/home_screen.js
@@ -6,6 +6,35 @@ const homeMangoScript = new MangoScript('homeUI', function(){
     imageSizeAuto: true,
     mergeImageUrl: false,
     img: SRC_HOME_BG_IMG.tag,
+    z: 0,
+  })
+  
+  var contact_icon = new entity({
+    type: 'image',
+    dWidth: 35,
+    dHeight: 35,
+    x: 379,
+    y: 119,
+    dx: 3,
+    dy: 3,
+    width: 50,
+    height: 50,
+    mergeImageUrl: false,
+    img: SRC_ICON_SHEET.tag,
+  })
+  
+  var settings_icon = new entity({
+    type: 'image',
+    dWidth: 35,
+    dHeight: 35,
+    x: 536,
+    y: 67,
+    dx: (window.innerWidth - 37),
+    dy: 3,
+    width: 50,
+    height: 50,
+    mergeImageUrl: false,
+    img: SRC_ICON_SHEET.tag,
   })
   
   background_image.data.onupdated = function() {
